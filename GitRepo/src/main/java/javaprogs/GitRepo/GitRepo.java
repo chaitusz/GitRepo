1d71bfb9d8b1d0757615a92a598074ae28587f23
Chaitanya <30646816+chaitusz@users.noreply.github.com>
2019-05-15 17:46:55 +0530

    First Commit

---
+++ GitRepo/src/main/java/javaprogs/GitRepo/GitRepo.java
@@ -0,0 +1,12 @@
+package javaprogs.GitRepo;
+
+public class GitRepo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is a git repo class");
+
+	}
+
+}
