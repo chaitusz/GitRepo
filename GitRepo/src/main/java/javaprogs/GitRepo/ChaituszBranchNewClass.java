6fa60b68d6028f26fd543c6bcfb09ea0e52872e0
Chaitanya <30646816+chaitusz@users.noreply.github.com>
2019-05-15 18:11:55 +0530

    local repo commit from chaitusz branch

---
+++ GitRepo/src/main/java/javaprogs/GitRepo/ChaituszBranchNewClass.java
@@ -0,0 +1,11 @@
+package javaprogs.GitRepo;
+
+public class ChaituszBranchNewClass {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("This is a local class from local branch chaitusz");
+		System.out.println("First added in the local repo");
+	}
+
+}
